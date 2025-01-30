1bb25c6c82087ed2072d351e598a2c7c548798bf
ernestknurov <ernknourov@gmail.com>
2025-01-30 13:42:53 +0100

    Finalized First Version
    - Add mutation: now after crossover each creature has a small chance to mutate its genome.
    - added temperature variable to generation of move probabilities. Temperature decides how sharply would be the difference between probs of each action.
    - Add methods to save and load population.
    - Add new strategy for crossover. Previously it was only replication, now you can also choose breeding, which means breed t creature together to generate new one.
    - Add some new select criteria

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import sys
+import json
 import time
 import copy
 import pygame
@@ -16,6 +17,8 @@ GRID_SIZE = 64
 POPULATION_SIZE = 500
 STEPS_PER_GENERATION = 200
 NUM_GENERATIONS = 100
+MUTATION_CHANCE = 0.05
+MUTATION_SCALE = 20
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 screen.fill(Color('white'))
@@ -67,10 +70,11 @@ class Creature:
     
     def _genome_to_move_probs(self, genome):
         g1, g2, g3 = genome
-        p1 = (g1 * 0.5 + g2 * 0.1 + g3 * 0.4) / 255.0
-        p2 = (g1 * 0.2 + g2 * 0.2 + g3 * 0.6) / 255.0
-        p3 = (g1 * 0.4 + g2 * 0.3 + g3 * 0.3) / 255.0
-        p4 = (g1 * 0.3 + g2 * 0.5 + g3 * 0.2) / 255.0
+        temperature = (g1 * 0.1 + g2 * 0.8 + g3 * 0.1) / 255.0
+        p1 = (g1 * 0.5 + g2 * 0.1 + g3 * 0.4) / 255.0 / temperature 
+        p2 = (g1 * 0.2 + g2 * 0.2 + g3 * 0.6) / 255.0 / temperature 
+        p3 = (g1 * 0.4 + g2 * 0.3 + g3 * 0.3) / 255.0 / temperature 
+        p4 = (g1 * 0.3 + g2 * 0.5 + g3 * 0.2) / 255.0 / temperature 
         probs = [p1, p2, p3, p4]
         softmax_probs = np.exp(probs)/np.sum(np.exp(probs))
         return softmax_probs
@@ -96,6 +100,20 @@ class Creature:
         if np.random.rand() < self.chance_to_move: 
             direction = np.random.choice(4, p=self.move_probs)
             self.move(direction_to_vector[direction])
+
+    @classmethod
+    def deserialize(cls, obj):
+        instance = cls(obj['location'], obj["genome"])
+        instance.chance_to_move = obj['chance_to_move']
+        return instance
+    
+    def serialize(self):
+        return {
+            "genome": self.genome.tolist(),
+            "move_probs": self.move_probs.tolist(),
+            "chance_to_move": self.chance_to_move,
+            "location": self.location.tolist()
+        }
 
 
 class Population:
@@ -127,20 +145,47 @@ class Population:
         after = len(self.population)
         return np.round(after / before, 3) * 100
 
-    def crossover(self):
+    def crossover(self, strategy="breeding"):
         old_population = self.population
         self.population = []
         self.grid.fill(-1)
         self.free_locations = np.argwhere(self.grid == -1)
-        parent_creature_ids = np.random.choice(len(old_population), self.size, replace=True)
-        for i in range(self.size):
-            location = self._get_location_without_collisions()
-            parent_creature = old_population[parent_creature_ids[i]]
-            genome = copy.deepcopy(parent_creature.genome)
-            child_creature = Creature(location, genome)
-            self.population.append(child_creature)
-            self.grid[*location] = i
 
+        if strategy == "copying":
+            parent_creature_ids = np.random.choice(len(old_population), self.size, replace=True)
+            for i in range(self.size):
+                location = self._get_location_without_collisions()
+                parent_creature = old_population[parent_creature_ids[i]]
+                genome = copy.deepcopy(parent_creature.genome)
+                child_creature = Creature(location, genome)
+                self.population.append(child_creature)
+                self.grid[*location] = i
+
+        elif strategy == "breeding":
+            old_population_size = len(old_population)
+            genome_length = len(old_population[0].genome)
+            for i in range(self.size):
+                mom_id, dad_id = np.random.choice(old_population_size, 2)
+                mom, dad = old_population[mom_id], old_population[dad_id]
+                mask = np.random.choice([0, 1], size=genome_length)
+                genome = np.where(mask, mom.genome, dad.genome)
+                location = self._get_location_without_collisions()
+                child_creature = Creature(location, genome)
+                self.population.append(child_creature)
+                self.grid[*location] = i
+
+        else:
+            raise ValueError("Unknown strategy type")
+        
+    def mutate(self, mutation_chance=0.001):
+        mutated_ids = np.argwhere(np.random.rand(self.size) < mutation_chance)
+        for id in mutated_ids:
+            old_genome = self.population[id[0]].genome.copy()
+            old_location = self.population[id[0]].location.copy()
+            noise = np.random.normal(loc=0, scale=MUTATION_SCALE, size=old_genome.shape)  # Gaussian noise
+            genome = np.clip(old_genome + np.round(noise), 0, 255)
+            self.population[id[0]] = Creature(old_location, genome)
+        return len(mutated_ids)
 
     def render(self, surface):
         for creature in self.population:
@@ -177,22 +222,51 @@ class Population:
         for i, creature in enumerate(self.population):
             self.grid[*creature.location] = i
 
-def select_criteria(creature):
+    def load_population(self, file_name):
+        with open(f"{file_name}.json", "r") as f:
+            population = json.load(f)['population']
+        self.population = [Creature.deserialize(creature) for creature in population]
+
+    def save_population(self, file_name="population"):
+        save_obj = {
+            "population_size": len(self.population),
+            "population": [creature.serialize() for creature in self.population]
+        }
+        with open(f"{file_name}.json", "w") as f:
+            json.dump(save_obj, f, indent=4)
+
+# SELECT CRITERIA
+def on_right_side(creature):
     return creature.location[1] >= GRID_SIZE // 2
+
+def on_left_side(creature):
+    return creature.location[1] <= GRID_SIZE // 2
+
+def in_the_corners(creature, radius=15):
+    corners = np.array([[0, 0], [0, GRID_SIZE -1], [GRID_SIZE - 1, 0], [GRID_SIZE - 1, GRID_SIZE - 1]])
+    return any(np.linalg.norm(creature.location - corner, ord=1) <= radius for corner in corners)
+
+def in_the_center(creature, radius=20):
+    center = np.array([GRID_SIZE // 2, GRID_SIZE // 2])
+    return np.linalg.norm(creature.location - center, ord=1) <= radius
 
 
 grid = Grid()
+select_criteria = on_right_side
 population = Population(POPULATION_SIZE)
+# population.load_population("final_population")
+population.save_population("initial_population")
 text = font.render(f'Gen 0: survival rate - 100%', False, (0, 0, 0))
 times = np.zeros(NUM_GENERATIONS * STEPS_PER_GENERATION)
 step = 0
 gen = 0
+
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
-            non_zero_times = times[times > 0]
-            mean = non_zero_times.mean()
+            mean = times[times > 0].mean()
             print("Mean update time:", mean)
+            population.save_population("final_population")
             pygame.quit()
             sys.exit()
         if event.type == KEYDOWN:
@@ -204,10 +278,17 @@ while True:
     
     elif step == STEPS_PER_GENERATION:
         survival_rate = population.select(select_criteria)
+
         start_time = time.time()
         population.crossover()
         end_time = time.time()
         print("Crossover time:", end_time - start_time)
+
+        start_time = time.time()
+        num_mutations = population.mutate(MUTATION_CHANCE)
+        end_time = time.time()
+        print("Mutation time:", end_time - start_time)
+
         gen += 1
         step = 0
         text = font.render(f'Gen {gen}: survival rate - {survival_rate}%', False, (0, 0, 0))
@@ -219,10 +300,9 @@ while True:
         times[gen * STEPS_PER_GENERATION + step] = end_time - start_time
         step += 1
 
-
     screen.fill(Color('white'))
     grid.render(screen)
     population.render(screen)
     screen.blit(text, (MARGIN, 0))
     pygame.display.update()
-    clock.tick(1000)
+    clock.tick(120)
